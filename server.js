2814347157f5cb443f66c1b1cd027562a317a62b
ken <15952039685@163.com>
2017-09-12 15:24:51 +0800

    mock test

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const Router = require('koa-router')
 const faker = require('faker');
 
 const Mock = require('mockjs');
+const Random = Mock.Random
 //home
 let home = new Router()
 home.get('/', async (ctx) => {
@@ -29,11 +30,17 @@ page.get('/faker',async (ctx)=>{
         randomCard:faker.helpers.createCard()
     }
 })
+Random.extend({
+    constellation: function(date) {
+        var constellations = ['白羊座', '金牛座', '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座', '水瓶座', '双鱼座']
+        return this.pick(constellations)
+    }
+})
 page.get('/mock',async (ctx)=>{
     ctx.body=Mock.mock({
         'list|1-10': [{
             // 属性 id 是一个自增数，起始值为 1，每次增 1
-            'id|+1': 1
+            'id|+1': '@CONSTELLATION'
         }]
     })
 })
